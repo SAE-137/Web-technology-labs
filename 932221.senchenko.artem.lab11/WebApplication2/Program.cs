4ee0682eb28adede1955bdfa182cddd04884cfd7
Artem Senchenko <moknechnes@mail.ru>
2025-11-26 13:44:53 +0700

    Lab 11 complete

--- 932221.senchenko.artem.lab11/WebApplication2/Program.cs
+++ 932221.senchenko.artem.lab11/WebApplication2/Program.cs
@@ -4,17 +4,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddTransient<WebApplication2.Services.ICalcService, WebApplication2.Services.CalcService>();
 
-
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -27,6 +23,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=CalcService}/{action=Index}/{id?}");
 
 app.Run();
