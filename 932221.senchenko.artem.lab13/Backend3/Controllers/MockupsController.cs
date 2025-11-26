90c21499ffb3dba943a759192c3c18311536399e
Artem Senchenko <moknechnes@mail.ru>
2025-11-26 14:46:01 +0700

    lab 13 (Quiz results does't work)

---
+++ 932221.senchenko.artem.lab13/Backend3/Controllers/MockupsController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Backend3.Models;
+
+namespace Backend3.Controllers
+{
+    public class MockupsController : Controller
+    {
+        private static readonly Random rnd = new Random();
+
+        private QuizModel GenerateNext(QuizModel model)
+        {
+            model.A = rnd.Next(1, 10);
+            model.B = rnd.Next(1, 10);
+            return model;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Quiz()
+        {
+            var model = new QuizModel
+            {
+                Step = 1
+            };
+
+            GenerateNext(model);
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Quiz(QuizModel model, string action)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            
+            bool correct = model.UserAnswer == model.CorrectAnswer;
+
+            
+            model.History.Add($"{model.A} + {model.B} = {model.UserAnswer}");
+
+            if (correct)
+                model.CorrectCount++;
+
+            
+            if (action == "finish" || model.Step >= 4)
+                return RedirectToAction("QuizResult", model);
+
+           
+            model.Step++;
+            model.UserAnswer = null;
+            GenerateNext(model);
+
+            return View(model);
+        }
+
+        public IActionResult QuizResult(QuizModel model)
+        {
+            return View(model);
+        }
+    }
+}
---
+++ 932221.senchenko.artem.lab13/Backend3/Models/QuizModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend3.Models
+{
+    public class QuizModel
+    {
+        
+        public int A { get; set; }
+        public int B { get; set; }
+
+        
+        [Required(ErrorMessage = "Введите ответ")]
+        public int? UserAnswer { get; set; }
+
+       
+        public int CorrectAnswer => A + B;
+
+        
+        public int Step { get; set; }
+
+       
+        public int CorrectCount { get; set; }
+
+       
+        public List<string> History { get; set; } = new List<string>();
+    }
+}
